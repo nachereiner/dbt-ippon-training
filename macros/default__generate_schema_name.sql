8e3aee3090e07ceef7621e33abe5e28051dbe2e2
Nicolas Achereiner <nachereiner@ippon.fr>
2023-11-21 09:17:53 +0000

    add macro generate schema name

---
+++ macros/default__generate_schema_name.sql
@@ -0,0 +1,14 @@
+{% macro default__generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+
+        {{ default_schema }}_{{ custom_schema_name | trim }}
+
+    {%- endif -%}
+
+{%- endmacro %}
